1d460d8c34604b46f1996ea9ee758ec7468d27da
WastingTime <egorov.sa@phystech.edu>
2024-10-29 23:54:11 +0300

    Add B solution

---
+++ 3.3 Algorithms/B.cpp
@@ -0,0 +1,13 @@
+template <typename Iter>
+Iter Unique(Iter first, Iter last) {
+    if (first == last) return last;
+
+    Iter result = first;
+    for (Iter it = std::next(first); it != last; ++it) {
+        if (*it != *result) {
+            ++result;
+            *result = *it;
+        }
+    }
+    return ++result;
+}
