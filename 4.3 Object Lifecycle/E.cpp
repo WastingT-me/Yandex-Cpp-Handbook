4656a4e41a17a73e7e140d50344e7bb6e955c74f
WastingTime <egorov.sa@phystech.edu>
2024-11-07 10:31:44 +0300

    Add logger

--- 4.3 Object Lifecycle/E.cpp
+++ 4.3 Object Lifecycle/E.cpp
@@ -2,10 +2,8 @@
 
 int main() {
     Logger log1;
-    Logger log2;
+    Logger* log2 = new Logger;
     Logger log3;
-    {
-        Logger temp = std::move(log2);
-    }
+    delete log2;
     return 0;
 }
